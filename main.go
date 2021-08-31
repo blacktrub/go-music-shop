7d598f8a60d83edc09b536b3e49ea013a7898d42
blacktrub <lightlocdog@gmail.com>
2021-08-31 21:20:30 +0300

    small refactoring

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type HttpError struct {
+	Error string `json:"error"`
+}
+
 type Storage interface {
 	Create() album
 	Read() album
@@ -73,16 +77,6 @@ func NewMemoryStorage() MemoryStorage {
 		{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 	}
 	return MemoryStorage{albums: albums}
-}
-
-type HttpError struct {
-	Error string `json:"error"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func getAlbums(c *gin.Context) {
